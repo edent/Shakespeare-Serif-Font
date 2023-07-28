fb323fb41ad93e8778aa4b10a3ff7260a99bf54b
Terence Eden <github.com@shkspr.mobi>
2023-07-28 09:17:01 +0100

    Add average letters

--- code/average.py
+++ code/average.py
@@ -1,16 +1,7 @@
 import os
 import numpy as np
-import argparse
 import math
-from PIL import Image
-
-def get_arguments():
-    ap = argparse.ArgumentParser()
-    ap.add_argument('-l', '--letter', type=str,
-                    help='The letter you want to average')
-    arguments = vars(ap.parse_args())
-
-    return arguments
+from PIL import Image, ImageChops
 
 def load_and_resize_images_from_directory(directory, target_size):
     image_files = [f for f in os.listdir(directory) if f.endswith(".png")]
@@ -58,26 +49,43 @@ def convert_to_1bpp(average_image, threshold=120):
 def save_1bpp_image(binary_image, output_path):
     # Convert the numpy array to a binary image
     binary_image = Image.fromarray(binary_image, mode="L")
+    
+    # Crop the whitespace around the image
+    binary_image = trim(binary_image)
 
     # Save the 1bpp monochrome image to the specified output path
     binary_image.save(output_path)
+    
+def trim(im):
+    bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+
 
 if __name__ == "__main__":
-    args = get_arguments()
-    letter = args['letter']
-    input_directory   = "../letters/" + letter + "/"
-    output_png_path = "../letters/" + letter + ".png"
-    target_size = (75, 75)  # Set the desired target size for resizing
+    #args = get_arguments()
+    #letter = args['letter']
+    letters = next(os.walk('../letters/raw/'))[1]
 
-    # Load, resize, and add border to all images from the directory
-    images = load_and_resize_images_from_directory(input_directory, target_size)
+    for letter in letters:
+        print("Averaging letter " + letter)
+        input_directory   = "../letters/raw/" + letter + "/"
+        if ( len(os.listdir(input_directory)) > 0 ):
+            output_png_path = "../letters/average/" + letter + ".png"
+            target_size = (200, 200)  # Set the desired target size for resizing
+        
+            # Load, resize, and add border to all images from the directory
+            images = load_and_resize_images_from_directory(input_directory, target_size)
 
-    # Calculate the average image
-    average_image = calculate_average_image(images)
+            # Calculate the average image
+            average_image = calculate_average_image(images)
 
-    # Convert the average image to 1bpp
-    binary_image = convert_to_1bpp(average_image)
-
-    # Save the 1bpp monochrome image
-    save_1bpp_image(binary_image, output_png_path)
+            # Convert the average image to 1bpp
+            binary_image = convert_to_1bpp(average_image)
+            
+            # Save the 1bpp monochrome image
+            save_1bpp_image(binary_image, output_png_path)
 
